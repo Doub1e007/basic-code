04c3dc94f0e44a5338456f0cf7c61c5a64897c99
Doub1e007 <3477848352@qq.com>
2025-03-05 22:19:21 +0800

    摸鱼ing

--- JavaSEpromax/day07-collections_map/src/com/doub1e/collection/CollectionsTest.java
+++ JavaSEpromax/day07-collections_map/src/com/doub1e/collection/CollectionsTest.java
@@ -13,7 +13,15 @@ public class CollectionsTest {
         //1.public static <T> boolean addAll(Collection<? super T> c, T...elements):为集合批量添加数据
         List<String> names = new ArrayList<>();
         Collections.addAll(names,"张无忌","周星驰","刘宇");
+        System.out.println(names);
+
+        //2.public static void shuffle(List<?> list) 打乱List集合中的元素顺序
+        Collections.shuffle(names);
+        System.out.println(names);
+
+        //3.public static <T> void sort(List<T> list)：对List集合中的元素进行升序排列
 
         List<Student> students = new ArrayList<>();
+        Student s1 = new Student("赵敏",19,169.5);
     }
 }
--- JavaSEpromax/day07-collections_map/src/com/doub1e/collection/Student.java
+++ JavaSEpromax/day07-collections_map/src/com/doub1e/collection/Student.java
@@ -1,4 +1,23 @@
 package com.doub1e.collection;
 
-public class Student {
+public class Student implements Comparable<Student>{
+    private String name;
+    private int age;
+    private double height;
+
+    //重写compare比较方法
+    //s1.compareTo(s2)
+    //左边对象 s1 == this
+    //右边对象 s2 == o
+    @Override
+    public int compareTo(Student o) {
+//        if (this.age > o.age){
+//            return 1;
+//        }else if (this.age < o.age){
+//            return -1;
+//        }
+//        return 0;
+//        //return this.age - o.age //升序
+        return o.age - this.age; //降序
+    }
 }
