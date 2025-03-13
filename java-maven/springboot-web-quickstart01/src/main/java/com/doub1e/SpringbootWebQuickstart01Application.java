f5e77dbe08dbeaec958d5d953d6909f1ddeb1edb
Doub1e007 <3477848352@qq.com>
2025-03-13 17:15:40 +0800

    spring relevant

---
+++ java-maven/springboot-web-quickstart01/src/main/java/com/doub1e/SpringbootWebQuickstart01Application.java
@@ -0,0 +1,16 @@
+package com.doub1e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class SpringbootWebQuickstart01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebQuickstart01Application.class, args);
+    }
+
+}
