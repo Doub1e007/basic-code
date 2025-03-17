ca15f951dae41bddb7ede7ca06ba432a5299bf28
Doub1e007 <3477848352@qq.com>
2025-03-17 10:52:48 +0800

    point

---
+++ java-maven/tlias-web-manager/src/main/java/com/doub1e/entity/Dept.java
@@ -0,0 +1,50 @@
+package com.doub1e.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.LocalDateTime;
+@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+
+public class Dept {
+    private Integer id;
+    private String name;
+    private LocalDateTime updateTime;
+
+    public Dept() {
+    }
+
+    public Dept(Integer id, String name, LocalDateTime updateTime) {
+        this.id = id;
+        this.name = name;
+        this.updateTime = updateTime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+}
