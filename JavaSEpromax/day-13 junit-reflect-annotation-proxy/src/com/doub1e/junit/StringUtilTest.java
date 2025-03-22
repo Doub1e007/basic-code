4fc3e0164f0cbccc5d97eaf733ecf7d2515ca14c
Doub1e007 <3477848352@qq.com>
2025-03-22 20:26:00 +0800

    junit

---
+++ JavaSEpromax/day-13 junit-reflect-annotation-proxy/src/com/doub1e/junit/StringUtilTest.java
@@ -0,0 +1,53 @@
+package com.doub1e.junit;
+
+import org.junit.*;
+
+public class StringUtilTest {
+    //private Connection con;
+    @Before //修饰实例方法 每个测试方法前都执行一次
+    public void before() {
+        //con = null;
+        System.out.println("===============before===============");
+    }
+
+    @After //修饰实例方法 每个测试方法后都执行一次
+    public void after() {
+        System.out.println("===============after===============");
+        //con.close();
+    }
+
+    @BeforeClass //修饰静态方法 全部测试方法前只执行一次
+    public static void beforeClass() {
+        System.out.println("===============beforeClass===============");
+    }
+
+    @AfterClass //修饰静态方法 全部测试方法后只执行一次
+    public static void afterClass() {
+        System.out.println("===============afterClass===============");
+    }
+
+    @Test
+    public void testPrintNumber(){
+        StringUtil.printNumber(null);
+        StringUtil.printNumber("");
+        StringUtil.printNumber("admin");
+    }
+    @Test
+    public void testGetMaxIndex(){
+//        System.out.println(StringUtil.getMaxIndex(null));
+//        System.out.println(StringUtil.getMaxIndex(""));
+//        System.out.println(StringUtil.getMaxIndex("admin"));
+
+        //断言
+        int i1 = StringUtil.getMaxIndex(null);
+        Assert.assertEquals("null测试失败",-1,i1);
+
+        int i2 = StringUtil.getMaxIndex("");
+        Assert.assertEquals("空字符串测试失败",-1,i2);
+
+        int i3 = StringUtil.getMaxIndex("admin");
+        Assert.assertEquals("admin测试失败",4,i3);
+
+        System.out.println("测试通过");
+    }
+}
