7879fd88f265fd19f91f44b2c1aa027e97b67c1a
Doub1e007 <3477848352@qq.com>
2025-03-23 23:58:10 +0800

    proxy

--- JavaSEpromax/day-13 junit-reflect-annotation-proxy/src/com/doub1e/proxy2/ProxyUtil.java
+++ JavaSEpromax/day-13 junit-reflect-annotation-proxy/src/com/doub1e/proxy2/ProxyUtil.java
@@ -5,8 +5,8 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class ProxyUtil {
-    public static UserService creatProxy(UserServiceImpl obj) {
-        UserService proxy = (UserService) Proxy.newProxyInstance(ProxyUtil.class.getClassLoader(), obj.getClass().getInterfaces(), new InvocationHandler() {
+    public static <T> T creatProxy(T obj) {
+        T proxy = (T) Proxy.newProxyInstance(ProxyUtil.class.getClassLoader(), obj.getClass().getInterfaces(), new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 long start = System.currentTimeMillis();
@@ -14,7 +14,7 @@ public class ProxyUtil {
                 Object result = method.invoke(obj, args);
 
                 long end = System.currentTimeMillis();
-                System.out.println("耗时：" + (end - start)/1000.0 + "s");
+                System.out.println(method.getName() + "方法耗时：" + (end - start)/1000.0 + "s");
                 return result;
             }
         });
--- JavaSEpromax/day-13 junit-reflect-annotation-proxy/src/com/doub1e/proxy2/UserServiceImpl.java
+++ JavaSEpromax/day-13 junit-reflect-annotation-proxy/src/com/doub1e/proxy2/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.doub1e.proxy2;
 public class UserServiceImpl implements UserService {
     @Override
     public void login(String loginName, String password) throws Exception {
-        long start = System.currentTimeMillis(); //记录开始时间 1970-1-1
 
         if("admin".equals(loginName) && "123456".equals(password)) {
             System.out.println("您登录成功，欢迎莅临本系统");
@@ -11,32 +10,20 @@ public class UserServiceImpl implements UserService {
             System.out.println("您登录失败，用户名或密码错误");
         }
         Thread.sleep(1000);
-
-        long end = System.currentTimeMillis();
-        System.out.println("login cost time: " + (end - start)/1000.0 + "s");
     }
 
     @Override
     public void deleteUser() throws Exception {
-        long start = System.currentTimeMillis();
-
         System.out.println("成功删除了1万个用户");
         Thread.sleep(1500);
-
-        long end = System.currentTimeMillis();
-        System.out.println("deleteUser cost time: " + (end - start)/1000.0 + "s");
     }
 
     @Override
     public String[] selectUser() throws Exception {
-        long start = System.currentTimeMillis();
 
         System.out.println("查询出了三个用户");
         String[] names = {"张全蛋","李二狗","牛爷爷"};
         Thread.sleep(500);
-
-        long end = System.currentTimeMillis();
-        System.out.println("selectUser cost time: " + (end - start)/1000.0 + "s");
         return names;
     }
 }
