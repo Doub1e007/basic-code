ab6f3a6c2ca3e56888e214b9aec1f9056a499220
Doub1e007 <3477848352@qq.com>
2025-05-29 14:16:29 +0800

    统一相应结果（bug版

---
+++ java-maven/tlias-web-manager/src/main/java/com/doub1e/TliasWebManagerApplication.java
@@ -0,0 +1,16 @@
+package com.doub1e;
+/**
+ * 启动类
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasWebManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasWebManagerApplication.class, args);
+    }
+
+}
