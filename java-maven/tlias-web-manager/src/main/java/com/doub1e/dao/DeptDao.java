ea74306708055ce2a261f75ca0168c388f49f411
Unspoken <96903993+Doub1e007@users.noreply.github.com>
2025-08-29 23:11:11 +0800

    Delete java-maven directory

--- java-maven/tlias-web-manager/src/main/java/com/doub1e/dao/DeptDao.java
+++
@@ -1,7 +0,0 @@
-package com.doub1e.dao;
-
-import java.util.List;
-
-public interface DeptDao {
-    public List<String> list();
-}
--- java-maven/tlias-web-manager/src/main/java/com/doub1e/service/DeptService.java
+++
@@ -1,9 +0,0 @@
-package com.doub1e.service;
-
-import com.doub1e.entity.Dept;
-
-import java.util.List;
-
-public interface DeptService {
-    public List<Dept> list();
-}
