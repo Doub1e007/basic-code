10ddf2966b20db957f251db3b073ddbdb89ebc2b
Doub1e007 <3477848352@qq.com>
2025-09-22 10:03:27 +0800

    登录拦截器实现

--- java-maven/tlias-web-manager/src/main/java/com/doub1e/filter/LoginCheckFilter.java
+++ java-maven/tlias-web-manager/src/main/java/com/doub1e/filter/LoginCheckFilter.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 
 @Slf4j
-@WebFilter("/*")
+//@WebFilter("/*")
 public class LoginCheckFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
